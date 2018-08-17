7695868d3c1a574273e0b51df83f1076227676f1
h.54isaac <ad123maso@gmail.com>
2018-08-17 10:39:31 +0300

    update

--- DS/vector/Stack_API.c
+++ DS/vector/Stack_API.c
@@ -1,15 +1,3 @@
-#ifndef __STACK_H__
-#define __STACK_H__
-#include "ADTDefs.h"
-#include "vector.h"
-#include<stddef.h>
-
-
-struct Stack
-{
-    Vector *m_vector;
-};
-
 Stack*  StackCreate( size_t _size, size_t _blockSize);
 void    StackDestroy(Stack* _stack);
 ADTErr  StackPush(   Stack* _stack, int  _item);
@@ -20,5 +8,7 @@ int     StackIsEmpty(Stack* _stack);
 /*  Unit-Test functions  */
 void    StackPrint(  Stack *_stack);
 
-
-#endif
+struct Stack
+{
+    Vector *m_vector;
+};
