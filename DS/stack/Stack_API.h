7ecd8636189eb10d5feea18cc36daa904b81ac7c
h54.isaac <ad123maso@gmail.com>
2018-08-19 00:57:14 +0300

    exSTACK

---
+++ DS/stack/Stack_API.h
@@ -0,0 +1,19 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+#include "ADTDefs.h"
+#include "vector.h"
+#include<stddef.h>
+
+typedef struct Stack Stack;
+
+Stack*  StackCreate( size_t _size, size_t _blockSize);
+void    StackDestroy(Stack* _stack);
+ADTErr  StackPush(   Stack* _stack, int  _item);  /* Add item to end. */
+ADTErr  StackPop(    Stack* _stack, int* _item);/* Remove from end. */
+ADTErr  StackTop(    Stack* _stack, int* _item);/* Return value from end. */
+int     StackIsEmpty(Stack* _stack);
+
+/*  Unit-Test functions  */
+void    StackPrint(  Stack *_stack);
+
+#endif
