d05d4c5dfc4997fdbb0fb7e64751c4b25c616164
h54.isaac <ad123maso@gmail.com>
2018-08-23 07:04:36 +0300

    update

--- DS/doublylinkedLIst/list.c
+++ DS/doublylinkedLIst/list.c
@@ -1,7 +1,6 @@
 #include "LIST.h"
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #define MAGIC 3567978
 #define IS_INVALID(l) (((l) == NULL) || l->m_tail.m_item != MAGIC)
 
@@ -29,68 +28,194 @@ List_t* ListCreate()
 	}
 	l->m_head.m_next=&(l->m_tail);
 	l->m_head.m_prev=NULL;
+	l->m_head.m_item;
 	l->m_tail.m_item=MAGIC;
-	l->m_tail.m_next=NULL;
 	l->m_tail.m_prev=&(l->m_head);
+	l->m_tail.m_next=NULL;
 	return l;
 }
-void NodeDestroy(Node *_node)
-{
-	if (_node == NULL)
-	{
-		return;
-	}
-	free(_node);
-}
-
+/*
+*/
 
 void   ListDestroy(List_t *_list)
-{
-	Node *cur,*nxt;
+{	
+	Node *nodePtrToFree;
+	if(IS_INVALID(_list))
+	{
+		return;
+	}	
+	nodePtrToFree=(Node*)malloc(sizeof(Node));
+	while (_list->m_head.m_next != &(_list->m_tail))
+	{
+		nodePtrToFree=(_list->m_head.m_next);
+		_list->m_head.m_next=_list->m_head.m_next->m_next;
+		free(nodePtrToFree);
+	}
+	free(_list);
+}
+
+/*
+	
+	
+	Node *cur,*lst_node,*nxt;
 	if(IS_INVALID(_list))
 	{
 		return;
 	}
-	cur=_list->m_head.m_next;
-	nxt=_list->m_head.m_next;
-	while(cur!=_list->m_tail.m_prev)
+	if(_list->m_m_head.m_next==&_list->m_tail)
 	{
-		nxt = cur->m_next;
+		free(_list);
+		return;
+	}
+	cur = _list->m_m_head.m_next;
+	nxt = cur;
+	lst_node=&_list->m_tail;
+	while(cur!=lst_node)
+	{
+		nxt=nxt->m_next;
 		free(cur);
 		cur=nxt;
 	}
 	free(_list);
 }
 	
-
-Node* NodeCreate(int _data)
+*/
+ADTErr NodeCreate(int _data,Node *_node)
 {
-	Node *newNode =(Node*)malloc(sizeof(Node));
+	Node *newNode;
+	if(_node == NULL)
+	{
+		return ERR_NOT_INITIALIZED;
+	}	
+	newNode =(Node*)malloc(sizeof(Node));
 	if (newNode == NULL)
 	{
-		return NULL;
+		return ERR_REALLOCATION_FAILED;
 	}
 	newNode->m_item=_data;
-	newNode->m_next=NULL;
-	newNode->m_prev=NULL;
-	return newNode;
+	newNode->m_next=_node->m_next;
+	newNode->m_prev=_node;
+	_node->m_next=newNode;
+	return ERR_OK;
 }
+	
+	
+
+
+ADTErr ListPushhead(List_t *_list, int  _item)
+{
+	if(IS_INVALID(_list))
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	return NodeCreate(_item,&(_list->m_head));
+}
+	
+ADTErr ListPushTail(List_t *_list, int  _item)
+{
+	if(IS_INVALID(_list))
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	
+	return NodeCreate(_item,_list->m_tail.m_prev);
+}
+		
+ADTErr NodeDestroy(Node *_node,int *_item)
+{
+	Node *dataHelder;
+	if (_node == NULL )
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	*_item=_node->m_item;
+	dataHelder=_node->m_next;
+	_node->m_next = dataHelder->m_next;
+	free(dataHelder);
+	return ERR_OK;
+}
+
+
+ADTErr ListPophead(List_t *_list, int *_item)
+{
+
+	if(IS_INVALID(_list) || _item == NULL)
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	if(_list->m_head.m_next==&(_list->m_tail))
+	{
+		return ERR_UNDERFLOW;
+	}
+	return NodeDestroy(_list->m_head.m_next,_item);
+}
+
+
+/*	
+	
+	Node* data_free;
+	if(IS_INVALID(_list) || _item == NULL)
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	*_item = _list->m_m_head.m_next->m_item;
+	data_free=_list->m_m_head.m_next->m.next;
+	_list->m_m_head.m_next=_list->m_m_head.m_next->m_next;
+	free(data_free);
+}
+*/
+ADTErr ListPopTail(List_t *_list, int *_item)
+{
+
+	if(IS_INVALID(_list) || _item == NULL)
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	if(_list->m_head.m_next==&(_list->m_tail))
+	{
+		return ERR_UNDERFLOW;
+	}
+	return NodeDestroy(_list->m_head.m_next,_item);
+}
+
+		
+
+
+
 /*
+
+	Node* data_free;
+	if(IS_INVALID(_list) || _item == NULL)
+	{
+		return ERR_NOT_INITIALIZED;
+	}
+	*_item = _list->m_m_head.m_next->m_item;
+	data_free=_list->m_m_head.m_next;
+	_list->m_m_head.m_next=_list->m_m_head.m_next->next;
+	free(data_free);
+*/	
+	
+
+
+/*
+size_t ListCountItems(List_t *_list);
+int    ListIsEmpty(const List_t *_list);
+
 ADTErr List_tRemove(List_t *_List_t, int *_item)
 {
-	Node *head_helder=(Node*)malloc(sizeof(Node));
+	Node *m_head_helder=(Node*)malloc(sizeof(Node));
 	if(IS_INVALID(_List_t) || _item == NULL)
 	{
 		return ERR_NOT_INITIALIZED;
 	}
-	if(_List_t->m_head == NULL)
+	if(_List_t->m_m_head == NULL)
 	{
 		return ERR_UNDERFLOW;
 	}
-	head_helder = _List_t->m_head;
-	_List_t->m_head = _List_t->m_head->m_next;
-	*_item =head_helder->m_item;
-	NodeDestroy(head_helder);
+	m_head_helder = _List_t->m_m_head;
+	_List_/t->m_m_head = _List_t->m_m_head.m_next;
+	*_item =m_head_helder->m_item;
+	NodeDestroy(m_head_helder);
 	return ERR_OK;
 } 
 	
@@ -102,9 +227,9 @@ ADTErr List_tInsert(List_t *_List_t, int  _item)
 	{
 		return ERR_NOT_INITIALIZED;
 	}
-	if(_List_t->m_head == NULL)
+	if(_List_t->m_m_head == NULL)
 	{
-		_List_t->m_head=newNode;
+		_List_t->m_m_head=newNode;
 		_List_t->m_tail=newNode;
 		return ERR_OK;
 	}
@@ -120,7 +245,7 @@ int  List_tIsEmpty(const List_t *_List_t)
 	{
 		return 1;
 	}
-	return _List_t->m_head==_List_t->m_tail;
+	return _List_t->m_m_head==_List_t->m_tail;
 }
 void List_tPrint(const List_t *_List_t)
 {
@@ -129,8 +254,8 @@ void List_tPrint(const List_t *_List_t)
 	{
 		return;
 	}
-	cur=_List_t->m_head;
-	nxt=_List_t->m_head;
+	cur=_List_t->m_m_head;
+	nxt=_List_t->m_m_head;
 	printf("(");
 	while(cur!=NULL)
 	{
