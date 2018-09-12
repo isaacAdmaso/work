7f754e85a87e71ea5dcf7ddc512e244e3864641b
isaac  admaso <ad123maso@gmail.com>
2018-09-12 18:04:10 +0300

    update

---
+++ DS/Hearts/Card.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+
+typedef enum Suite{Spades, Diamonds, Clubs, Hearts}Suite;
+typedef enum Rank {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+		    Jack, Queen, King, Ace}Rank;
+
+
+
+
+
+
