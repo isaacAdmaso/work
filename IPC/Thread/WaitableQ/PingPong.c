8c9061df7015a1db781f20a5448ad79e52d09c0b
h54.isaac <ad123maso@gmail.com>
2018-11-06 19:24:59 +0200

    update

--- IPC/Thread/WaitableQ/PingPong.c
+++ IPC/Thread/WaitableQ/PingPong.c
@@ -1,4 +1,6 @@
-#define _DEFAULT_SOURCE /**<-? */
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 500
+#endif
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,91 +9,87 @@
 #include <sys/types.h>
 #include <time.h>
 #include <assert.h>
-#include <semaphore.h>
 
 #include "Queue.h"
 #define MAX 4
 #define STR_OPT "vc:p:q:n:s:"
 
 const char USAGE[]	            ="\nUsage: %s [-c] [-p] [-q] [-v] [-n number of messages] [-s msec]\n";
-const char ERR_SEMA[]           = "\nERROR getting semaphore (F/E)!\n";
 const char ECHO_CONSUMER_FIN[]	="\ngoodbye from thread #%d count of msg removed: %d \n";
 const char ECHO_PRODUCER_FIN[]	="\ngoodbye from thread #%d,count of msg inserted: %d \n";
 const char ECHO_INSERT[]	    ="\nthread #%ld Insert succesfully\n";
-const char ECHO_REMOVE[]	    ="\nthread #%ld Remove succesfully\n";
+const char ECHO_REMOVE[]	    ="\nthread #%ld Remove msg No%d from thread #%d succesfully\n";
+
+union Msg
+{
+    short m_idTh;
+    short m_nMsg;
+    int m_toSend;
+};
 
 typedef struct S_data
 {
     Queue* m_myQ;
-    sem_t *m_empty,*m_full;
-    pthread_mutex_t *m_myMutex;
     int m_nOfItems;
-    int m_sleepTime,m_count;
+    int m_sleepTime;
+    int m_count;
+    union Msg m_msg;
 
 }S_data;
 
 
 void* Producers_func(void* _sData)
 {
-    int i;
+    short i;
     S_data* data = _sData; 
     pthread_t id = pthread_self();
 
     for(i = 0;i < data->m_nOfItems;++i)
     {
-        sem_wait(data->m_empty);
-        pthread_mutex_lock(data->m_myMutex);
-        QueueInsert(data->m_myQ,MAX);
+        data->m_msg.m_nMsg = i;
+        QueueInsert(data->m_myQ,data->m_msg.m_toSend);
         fprintf(stdout,ECHO_INSERT,id);
-        pthread_mutex_unlock(data->m_myMutex);
-        sem_post(data->m_full);
         data->m_count++;
         usleep(data->m_sleepTime);
+        QueuePrint(data->m_myQ);
     }
     return NULL;
 }
 
 void* Consumers_func(void* _sData)
 {
-    int max = 0;
+    union Msg max;
     S_data* data = _sData; 
     pthread_t id = pthread_self();
 
     for(;;)
     {
-        sem_wait(data->m_full);
-        pthread_mutex_lock(data->m_myMutex);
-        QueueRemove(data->m_myQ,&max);
-        fprintf(stdout,ECHO_REMOVE,id);
-        pthread_mutex_unlock(data->m_myMutex);
-        sem_post(data->m_empty);
+        QueuePrint(data->m_myQ);
+        QueueRemove(data->m_myQ,&(max.m_toSend));
+        fprintf(stdout,ECHO_REMOVE,id,max.m_nMsg,max.m_idTh);
         usleep(data->m_sleepTime);
         data->m_count++;
     }
     return NULL;
 }
 
-void Data_Init(S_data* _thData,sem_t* _empty,sem_t* _full,Queue* _myQ
-                ,int _nOfItems,int _sleepTime,pthread_mutex_t* _myMutex)
+void Data_Init(S_data* _thData,Queue* _myQ,int _nOfItems,int _sleepTime,short _id)
 {
-    _thData->m_empty = _empty;
-    _thData->m_full = _full;
-    _thData->m_myMutex = _myMutex;
     _thData->m_myQ = _myQ;
     _thData->m_nOfItems = rand()%_nOfItems + 1;
     _thData->m_sleepTime = rand()%_sleepTime; 
     _thData->m_count = 0;
+    _thData->m_msg.m_idTh = _id;
 }
 
 int main(int argc, char *argv[])
 {
-    int opt = -1,verbose = 0,consumers = MAX/2,producers = MAX/2,nOfItems = MAX,sleepTime = MAX,i;
-    size_t qSize = MAX/2,totSize = MAX;
+    int opt = -1,verbose = 0,consumers = MAX/2,producers = MAX/2,nOfItems = MAX,sleepTime = MAX;
+    short i;
+    size_t qSize = MAX/2,totAmount = MAX;
     Queue* myQ = NULL;
     pthread_t* thArr;
     S_data *thDataArr;
-    sem_t empty,full;
-    pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
     extern char *optarg;
 	extern int optind;
 
@@ -122,24 +120,15 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 	}
+
     if (verbose)
 		fprintf(stdout, "\ninit\n");
     myQ = QueueCreate(qSize);
     assert(myQ != NULL);
-    if(sem_init(&empty, 0, qSize))
-    {
-        fprintf(stderr,ERR_SEMA);
-        return 1;
-    }
-    if(sem_init(&full, 0, 0))
-    {
-        fprintf(stderr, ERR_SEMA);
-        return 1;
-    }
-
-    totSize = consumers + producers;
-    if(NULL == (thArr = malloc(totSize*sizeof(pthread_t))) ||
-         NULL == (thDataArr = malloc(totSize*sizeof(S_data))))
+    
+    totAmount = consumers + producers;
+    if(NULL == (thArr = malloc(totAmount*sizeof(pthread_t))) ||
+         NULL == (thDataArr = malloc(totAmount*sizeof(S_data))))
     {
         fprintf(stderr, "\nError malloc\n");
         return 1;
@@ -147,16 +136,16 @@ int main(int argc, char *argv[])
     srand (time(0));
     for(i = 0;i < producers;++i)
     {
-        Data_Init(thDataArr + i,&empty,&full,myQ,nOfItems,sleepTime,&myMutex);
+        Data_Init(thDataArr + i,myQ,nOfItems,sleepTime,i);
         if(pthread_create(&thArr[i], NULL, Producers_func,&thDataArr[i]))
         {
             fprintf(stderr, "\nError creating thread\n");
             return 1;
         }
     }
-    for(;i < totSize;++i)
+    for(;i < totAmount;++i)
     {
-        Data_Init(thDataArr + i,&empty,&full,myQ,nOfItems,sleepTime,&myMutex);
+        Data_Init(thDataArr + i,myQ,nOfItems,sleepTime,i);
         if(pthread_create(&thArr[i], NULL, Consumers_func,&thDataArr[i]))
         {
             fprintf(stderr, "Error creating thread\n");
@@ -165,6 +154,7 @@ int main(int argc, char *argv[])
     }
     if (verbose)
 		fprintf(stdout, "\njoin\n");
+    QueuePrint(myQ);
     for(i = 0; i < producers;++i)
     {
         if(pthread_join(thArr[i], NULL))
@@ -174,7 +164,9 @@ int main(int argc, char *argv[])
         }
         fprintf(stdout,ECHO_PRODUCER_FIN,i,thDataArr[i].m_count);
     }
-    for(; i < totSize;++i)
+    QueuePrint(myQ);
+
+    for(; i < totAmount;++i)
     {
         if(pthread_cancel(thArr[i]))
         {
@@ -183,6 +175,7 @@ int main(int argc, char *argv[])
         }
         fprintf(stdout,ECHO_CONSUMER_FIN,i,thDataArr[i].m_count);
     }
+    QueuePrint(myQ);
     free(thArr);
     free(thDataArr);
     return 0;
