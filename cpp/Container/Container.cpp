14ff6650f6d09fe69a3412374a23e1566348e7b3
h54.isaac <ad123maso@gmail.com>
2018-12-11 08:58:15 +0200

    u

---
+++ cpp/Container/Container.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file Container.cpp
+ * @brief 
+ * @version 0.1
+ * @date 2018-12-11
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+#include <iostream>
+#include "Container.h"
+
+using namespace std;
+
+int main()
+{
+    Container_t<int, vector<int*> >  	iArr;    
+    return 0;
+}
---
+++ cpp/Container/Container.h
@@ -0,0 +1,88 @@
+/**
+ * @file Container.h
+ * @brief 
+ * @version 0.1
+ * @date 2018-12-10
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+#ifndef __CONTAINER_H__
+#define __CONTAINER_H__
+#include <typeinfo>
+#include <algorithm>    // std::find_if
+#include <list>
+#include <vector>
+
+using namespace std;
+template<class T,class Container>
+//template<typename T,template <typename, typename = std::allocator<T*> > class Container>
+
+class Container_t
+{
+
+    typedef typename Container::iterator	iter_t;
+
+private:
+    Container      myContainer;
+    Container_t(Container_t& _c){}
+    Container_t& operator = (const Container_t _c);
+    T helper(size_t _idx)    {
+        if(_idx >= myContainer.size())
+            return *(myContainer.begin());
+        if(typeid(myContainer) == typeid(vector<T*>)){
+            return *(myContainer[_idx]);
+        }else if(typeid(myContainer) == typeid(list<T*>)){
+            iter_t it = myContainer.begin();
+            while(it != myContainer.end()){if(_idx == 0) return *(*it); --_idx; ++it;}
+            return *(myContainer.begin());
+        }
+    }
+    struct find_n{
+        T _item;
+        bool operator ()(T* _ptr)const {return *_ptr > _item;}
+    };
+
+
+
+public:
+    Container_t(){}
+    ~Container_t(){}
+
+    bool isEmpty(){return myContainer.empty();}
+    size_t nItems(){return myContainer.size();}
+    void    insertNew(T& _item){return myContainer.push_back(&_item);}
+    T*      firstItem(){return myContainer.front();}
+    T*      lastItem(){return myContainer.back();}
+    T*      findItem(T& _item)
+    {
+        find_n toFind;
+        toFind._item = _item;
+        iter_t it = find_if (myContainer.begin(), myContainer.end(), toFind());
+        if(it == myContainer.end())
+            return 0;
+        return *it;
+    }
+    T*      eraseItem(T _item)
+    {
+        find_n toFind;
+        toFind._item = _item;
+        iter_t it2,it1 = find_if (myContainer.begin(), myContainer.end(), toFind());
+        if(it1 == myContainer.end())
+            return 0;
+        it2 = myContainer.erase(it1);
+        return *it2;
+    }
+    void    eraseAll(){myContainer.erase(myContainer.begin(), myContainer.end());}
+    T       operator [] (size_t _idx){return helper(_idx);}
+
+};
+
+
+  
+
+
+
+
+
+#endif //__CONTAINER_H__
