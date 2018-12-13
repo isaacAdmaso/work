44bcf896ebc6fecd74ac755e8d1741112aa236d1
h54.isaac <ad123maso@gmail.com>
2018-12-13 13:43:46 +0200

    parser

--- cpp/Parser/main.cpp
+++ cpp/Parser/main.cpp
@@ -7,4 +7,25 @@
  * @copyright Copyright (c) 2018
  * 
  */
+#include <iostream>
 #include "parser.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    if(argc > 2)
+    {
+        cout << "ERR:no excutable file!!!";
+        return 1;
+    }
+    for(int i = 0; i < argc -1; ++i)
+    {
+        //code//
+    }
+
+
+    return 0;
+}
+
