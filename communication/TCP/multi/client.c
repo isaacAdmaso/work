0e0e14c2a6144331747c36c10a161480ac60b260
h54.isaac <ad123maso@gmail.com>
2018-12-24 10:50:16 +0200

    temp

--- communication/TCP/multi/client.c
+++ communication/TCP/multi/client.c
@@ -29,55 +29,90 @@
 
 const char clientMsg[] =  "Hello from client #%d\n";
 
-void SeAndRe(int sockfd,int clientN, char* msg,CS_t cs);
+void SeAndRe(int* sockfd,int clientN, char* msg,CS_t cs);
+void ClientInit(int* sockfdArr,struct sockaddr_in*	 servaddr,int argc,char* argv[]);
+void ConnectServer(int* sockfd, struct sockaddr_in*  servaddr);
+void ClientRun(int* sockfd, char* msg,int numC);
+
 
 
 int main(int argc,char* argv[]) { 
-	int sockfdArr[MAXAMOUNT],i,randomN = -1,rtVal; 
-	Addr addr = {PORT,IP};
+	int sockfdArr[MAXAMOUNT],i; 
 	char msg[MAXLINE]; 
 	struct sockaddr_in	 servaddr; 
-	CS_t cs = CLIENT;
 
-	memset(&servaddr, 0, sizeof(servaddr)); 
-	InitAdd(&servaddr,&addr,argc,argv);
-    for(i = 0;i < MAXAMOUNT; ++i)
-        sockfdArr[i] = DIS;
-
+    ClientInit(sockfdArr, &servaddr, argc, argv);
+	
     srand(time(NULL));
     while(1){
         for(i = 0;i < MAXAMOUNT; ++i){
-            randomN = rand()%10;
-            if( (sockfdArr[i] == DIS) ){
-                if(randomN < 3){
-                    rtVal = Conn(&(sockfdArr[i]),&servaddr);
-                    if(!HANDLE_ERR_NO_EXIT(rtVal < 0,rtVal,"connection")){
-                    	printf("connected to ip: %s\tport: %d\n",inet_ntoa(servaddr.sin_addr)
-                            ,ntohs(servaddr.sin_port));
-                    }
-                }
-            }else if( sockfdArr[i] > CONN ){
-                if(randomN < 1){
-                    close(sockfdArr[i]);
-                    sockfdArr[i] = -1; 
-                }else if(randomN < 4){
-                    SeAndRe(sockfdArr[i],i, msg, cs);
-                }
-            }
+            ConnectServer(sockfdArr + i,&servaddr);
+            ClientRun(sockfdArr + i,msg,i);
         }
     }
 	return 0; 
-} 
+}
 
-void SeAndRe(int sockfd,int clientN, char* msg,CS_t cs){
+
+
+
+
+void ClientInit(int* sockfdArr,struct sockaddr_in*	 servaddr,int argc,char* argv[]){
+	Addr addr = {PORT,IP};
+    int i;
+
+    memset(servaddr, 0, sizeof(*servaddr)); 
+	InitAdd(servaddr,&addr,argc,argv);
+    for(i = 0;i < MAXAMOUNT; ++i)
+        sockfdArr[i] = DIS;
+
+}
+
+void SeAndRe(int* sockfd,int clientN, char* msg,CS_t cs){
     int rtVal;
     snprintf ( msg, MAXLINE, clientMsg,clientN);
     fwrite (msg , sizeof(char), strlen(msg), stdout);
 
-    rtVal = Send(sockfd,msg,strlen(msg),cs);
+    rtVal = Send(*sockfd,msg,strlen(msg),cs);
     HANDLE_ERR_EXIT(rtVal < 0,rtVal,"send");
-
-    rtVal = Rec(sockfd,cs);
+    if(rtVal <= 0){
+        close(*sockfd);
+        *sockfd = -1;
+    }
+    rtVal = Rec(*sockfd,cs);
     HANDLE_ERR_EXIT(rtVal < 0,rtVal,"receive");
+    if(rtVal == 0){
+        close(*sockfd);
+        *sockfd = -1;
+    }
 }
 
+void ConnectServer(int* sockfd, struct sockaddr_in*  servaddr){
+    int rtVal,randomN;
+
+    randomN = rand()%10;
+    if( (*sockfd == DIS) ){
+        if(randomN < 3){
+            rtVal = Conn(sockfd,servaddr);
+            if(!HANDLE_ERR_NO_EXIT(rtVal < 0,rtVal,"connection")){
+                printf("connected to ip: %s\tport: %d\n",inet_ntoa(servaddr->sin_addr)
+                    ,ntohs(servaddr->sin_port));
+            }
+        }
+    }
+}
+
+void ClientRun(int* sockfd, char* msg,int numC){
+    int randomN;
+	CS_t cs = CLIENT;
+
+    randomN = rand()%10;
+    if( *sockfd > CONN ){
+        if(randomN < 1){
+            close(*sockfd);
+            *sockfd = -1; 
+        }else if(randomN < 4){
+            SeAndRe(sockfd,numC, msg, cs);
+        }
+    }
+}
--- communication/errorHandle.c
+++ communication/errorHandle.c
@@ -14,7 +14,7 @@
 
 
 int HandleError(int _cond, int _rtval, char* _msg, char* fileN, int lineN, int _errnoN){
-    if(_cond){
+   /* if(_cond){
         printf("Error in: %s\nrtval is: %d\nin file: %s line %d\n",
             _msg,_rtval,fileN,lineN);
         
@@ -22,6 +22,6 @@ int HandleError(int _cond, int _rtval, char* _msg, char* fileN, int lineN, int _
     }else{
         printf("rtval from %s is %d\n",_msg,_rtval);
     }
-    fflush(stdout);
+    fflush(stdout);*/
     return _cond;
 }
