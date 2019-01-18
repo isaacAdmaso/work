f393e82a19a03907491683427e9836106ec9de35
h54.isaac <ad123maso@gmail.com>
2019-01-18 10:52:51 +0200

    fin factory

--- advance_cpp/projectSmartHome/Factory/AgentFactory.cpp
+++ advance_cpp/projectSmartHome/Factory/AgentFactory.cpp
@@ -1,53 +1,31 @@
+/**
+ * @file AgentFactory.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-01-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "AgentFactory.h"
 #include <dlfcn.h>
 #include <string>
 
-AgentFactory::AgentFactory(IServer* _server)
-:m_server(_server)
-{
-}
+const std::string AgentFactory::libPrefix = "../lib/lib";
+const std::string AgentFactory::libSuffix = ".so";
+
 AgentFactory::~AgentFactory()
 {
 }
 IAgent* AgentFactory::MakeAgent(const IAgent::AgentConfig& _agentConfig)
 {
-	m_server = _agentConfig.m_server;
-	std::string libPath = _agentConfig.m_type;
-	libPath = "./" + libPath + ".so";
-	void* handler = dlopen(libPath.c_str(), RTLD_LAZY);
-	//error
-	//make ctor
-	IAgent* (*createAgent)();
-	createAgent = (IAgent* (*)())dlsym(handler, "Create");
-	IAgent* agent = createAgent();
-	agent->Init(_agentConfig);
+	Create createAgent;
+	std::string libPath = libPrefix + _agentConfig.m_id + libSuffix;
+
+	void* handle = dlopen(libPath.c_str(), RTLD_LAZY);
+	createAgent = (Create)dlsym(handle, "Create");
+	IAgent* agent = createAgent(_agentConfig);
+	//agent->Init();
 	return agent;
-	//make init
 }
-/*
-	void * handler = dlopen("./x.so", RTLD_LAZY);
-	Y* (*funcCreate)(int,int);
-	
-	funcCreate = (Y*(*)(int,int))dlsym(handler,"create");
-	Y* y = funcCreate(4,5);
-	y->Print();
-/*
-	void *handle;
-	double (*cosine)(double,double);
-	char *error;
-
-	handle = dlopen("../../../lib/libADD.so", RTLD_LAZY);
-	if(!handle)
-	{
-		fprintf(stderr, "%s\n", dlerror());
-/*		exit(EXIT_FAILURE);
-	}
-
-	dlerror();
-
-	cosine=(double(*)(double,double))dlsym(handle, "Calc");
-	
-	printf("%f\n",cosine(5,6));
-	
-	 dlclose(handle);
-	 */
--- advance_cpp/projectSmartHome/Factory/AgentFactory.h
+++ advance_cpp/projectSmartHome/Factory/AgentFactory.h
@@ -1,16 +1,32 @@
+/**
+ * @file AgentFactory.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-01-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef __AGENTFACTORY__
 #define __AGENTFACTORY__
-
-#include "IServer.h"
+#include <string>
 #include "IAgent.h"
-class AgentFactory: private UncopyAble
+#include "IServer.h"
+#include "uncopy.h"
+
+
+class AgentFactory: private Uncopy
 {
-public :
-	AgentFactory(IServer* _server);
+public:
 	~AgentFactory();
 	IAgent* MakeAgent(const IAgent::AgentConfig& _agentConfig);
+
 private:
-	IServer* m_server;
-	//messageQ;
+	typedef IAgent* (*Create)(const IAgent::AgentConfig&);
+	static const std::string libPrefix;
+	static const std::string libSuffix;
+	//IServer* m_server;
 };
-#endif
+
+#endif //__AGENTFACTORY__
