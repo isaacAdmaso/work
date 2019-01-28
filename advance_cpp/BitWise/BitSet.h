cf7245e1ae59868e1e5fa60aca7afc899b96e766
h54.isaac <ad123maso@gmail.com>
2019-01-28 07:04:47 +0200

    fin step one

--- advance_cpp/BitWise/BitSet.h
+++ advance_cpp/BitWise/BitSet.h
@@ -13,11 +13,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
+#include <climits>//CHAR_BIT
 
 
-#define S(T)           sizeof(T)*CHAR_BIT
-#define NELEMENT(x,y) ((x)%(y))?((N / y) + 1):(N / y)
+#define S(T)           (sizeof(T)*CHAR_BIT)
+#define NELEMENT(x,y) (((x)%(y))?((x / y) + 1):(x / y))
 namespace Advance_Cpp
 {
 
@@ -26,14 +26,14 @@ template<size_t N,typename T  = size_t>
 class BitSet
 {
 public:
-    //BitSet();
-    //~BitSet();
+    BitSet();
     BitSet(const BitSet& _bs);
+	BitSet& operator=(const BitSet& _bs);
     explicit BitSet(const std::vector<bool>& _boolArray);
-    std::ostream& Print(std::ostream& _os)const;
+    void Print(std::ostream& _os)const;
 
-	BitSet& operator= (const BitSet& _bs); 
 	BitSet& operator|=(const BitSet& _bs);
+	BitSet& operator^=(const BitSet& _bs);
 	BitSet& operator&=(const BitSet& _bs);
 	BitSet& operator<<=(size_t _nos); 
 	
@@ -46,10 +46,201 @@ public:
 	bool any() const;
 private:
     T m_Bit[ NELEMENT(N,S(T))];
+	T* begin();
+	T* end();
+	const T* begin() const;
+	const T* end() const;
+};
+
+
+template<size_t N,typename T>
+T* BitSet<N,T>::begin()
+{
+	return m_Bit;
+}
+
+template<size_t N,typename T>
+T* BitSet<N,T>::end()
+{
+	return m_Bit+NELEMENT(N,S(T));
+}
+
+
+
+template<size_t N,typename T>
+const T* BitSet<N,T>::begin() const
+{
+	return m_Bit;
+}
+
+template<size_t N,typename T>
+const T* BitSet<N,T>::end() const
+{
+	return m_Bit+NELEMENT(N,S(T));
+}
+
+template<size_t N,typename T>
+BitSet<N,T>::BitSet()
+{
+	std::fill(begin(),end(),0); 
+}
+
+template<size_t N,typename T>
+BitSet<N,T>::BitSet(const BitSet& _bs)
+{
+	std::copy ( _bs.begin(), _bs.end(), begin()); 
+}
+
+
+template<size_t N,typename T>
+BitSet<N,T>& BitSet<N,T>::operator=(const BitSet& _bs)
+{
+	if(*this != _bs)
+	{
+		std::copy ( _bs.begin(), _bs.end(),begin()); 
+	}
+	return *this;
+}
+
+
+template<size_t N,typename T>
+BitSet<N,T>::BitSet(const std::vector<bool>& _boolArray)
+{
+    size_t size = _boolArray.size();
+
+    for(size_t i = 0; i < size; i+=S(T))
+    {
+        T c = 0;
+        for(size_t j = 0 ; (j < S(T))&&(j < (size - i)); ++j)
+        {
+            if(_boolArray[i + j])
+                c |= T(1<<(S(T) - 1 - j)); 
+
+        }
+        m_Bit[i/S(T)] = c;
+    }
+}
+
+namespace BitSet_imp
+{
+
+template<typename T>
+struct Or
+{
+    T operator()(const T& m_bit,const T& _otherBs)
+    {
+        return m_bit | _otherBs;
+    }
+
+};
+
+
+template<typename T>
+struct XOr
+{
+    T operator()(const T& m_bit,const T& _otherBs)
+    {
+        return m_bit ^ _otherBs;
+    }
+
+};
+
+
+template<typename T>
+struct And
+{
+    T operator()(const T& m_bit,const T& _otherBs)
+    {
+        return m_bit & _otherBs;
+    }
+
+};
+
+
+template<typename T>
+class Print
+{
+public:
+    Print(std::ostream& _os)
+    :m_os(_os){}
+    void operator()(const T& _val)const
+    {
+        for(int i = S(T) - 1; i >= 0; --i )
+            m_os << ((_val & (T(1) << i))? "1" : "0") <<"|";
+    }
+private:
+    std::ostream& m_os;
 };
 
 }
 
-#include "BitSet.hpp"
+template<size_t N,typename T>
+BitSet<N,T>& BitSet<N,T>::operator|= (const BitSet& _bs)
+{
+  	std::transform (begin(), end(), _bs.begin(),begin(), BitSet_imp::Or<T>());
+  	return *this;
+}
+
+
+template<size_t N,typename T>
+BitSet<N,T>& BitSet<N,T>::operator^= (const BitSet<N,T>& _bs)
+{
+  	std::transform (begin(), end(), _bs.begin(),begin(), BitSet_imp::XOr<T>());
+  	return *this;
+}
+
+
+template<size_t N,typename T>
+BitSet<N,T>& BitSet<N,T>::operator&= (const BitSet& _bs)
+{
+  	std::transform (begin(), end(), _bs.begin(),begin(), BitSet_imp::And<T>());
+  	return *this;
+}
+
+
+template<size_t N,typename T>
+void BitSet<N,T>::Print(std::ostream& _os)const
+{
+    std::for_each(begin(),end(),BitSet_imp::Print<T>(_os));
+}
+
+
+template<size_t N,typename T>
+std::ostream& operator<<(std::ostream& _os, const BitSet<N,T>& _bs)
+{
+	_bs.Print(_os);
+    return _os; 
+}
+
+
+template<size_t N,typename T>
+bool BitSet<N,T>::Get(size_t _index) const
+{
+  return (m_Bit[_index / S(T)] & (T(1) << (_index % S(T))));
+}
+
+
+template<size_t N,typename T>
+void BitSet<N,T>::Set(size_t _index)
+{
+  m_Bit[_index/S(T)] |= T(1 << (_index%S(T)));
+}
+
+template<size_t N,typename T>
+void BitSet<N,T>::Clear(size_t _index) 
+{
+  	m_Bit[_index/S(T)] &= T(~(1 << (_index%S(T))));
+}
+
+
+template<size_t N,typename T>
+void BitSet<N,T>::Flip(size_t _index) 
+{
+  	m_Bit[_index/S(T)] ^= T(1 <<(_index%S(T)));
+}
+
+}
+
+//#include "BitSet.hpp"
 
 #endif //__BITWISE_H__
